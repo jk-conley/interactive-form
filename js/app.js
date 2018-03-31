777fdd9f33e3819df7d0bf0fdb492e31f813feab
James_K_Conley <jkconleydec2012@gmail.com>
2018-03-31 13:17:12 -0700

    Added the t-shrit funtionality, which selects various colors based on design choice

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ VARIABLES
 const nameField = document.getElementById('name');
 const basicInfoField = document.getElementById('basic-info');
 const selectJobRole = document.getElementById('title');
+const selectShirtDesign = document.getElementById('design');
 
 // Elements created with js
 const div = document.createElement('div');
@@ -56,11 +57,11 @@ const createTextField = () => {
 
 }
 
-/*==================================
-CHECK WHICH OPTION IS SELECTED
-==================================*/
+/*=========================================
+CHECK WHICH JOB ROLE OPTION IS SELECTED
+=========================================*/
 
-const optionSelected = (option) => {
+const jobRoleOptionSelected = (option) => {
 
   // assign textarea if present by id
   const textArea = document.getElementById('other-title');
@@ -76,10 +77,101 @@ const optionSelected = (option) => {
   }
 }
 
+/*===================================================
+*** T-SHIRT INFO FUNCTIONS ***
+===================================================*/
 
-/*=============================
-DOMContentLoaded Call Functions
-=============================*/
+/*==================================
+JS PUNS COLORS FUCNTION
+==================================*/
+
+const jsPunsColors = (colors) => {
+
+  // sort which colors to show for design js puns
+
+  for (let i = 0; i < colors.length; i++) {
+    switch (colors[i].value) {
+      case 'cornflowerblue':
+        colors[i].style.display = '';
+        colors.value = colors[i].value;
+        break;
+      case 'darkslategrey':
+        colors[i].style.display = '';
+        break;
+      case 'gold':
+        colors[i].style.display = '';
+        break;
+      case 'tomato':
+        colors[i].style.display = 'none';
+        break;
+      case 'steelblue':
+        colors[i].style.display = 'none';
+        break;
+      case 'dimgrey':
+        colors[i].style.display = 'none';
+        break;
+    }
+  }
+
+}
+
+/*==================================
+HEART JS COLORS FUNCTION
+==================================*/
+
+const heartJsColors = (colors) => {
+
+  // sort which colors to show for design heart js
+
+  for (let i = 0; i < colors.length; i++) {
+    switch (colors[i].value) {
+      case 'cornflowerblue':
+        colors[i].style.display = 'none';
+        break;
+      case 'darkslategrey':
+        colors[i].style.display = 'none';
+        break;
+      case 'gold':
+        colors[i].style.display = 'none';
+        break;
+      case 'tomato':
+        colors[i].style.display = '';
+        colors.value = colors[i].value;
+        break;
+      case 'steelblue':
+        colors[i].style.display = '';
+        break;
+      case 'dimgrey':
+        colors[i].style.display = '';
+        break;
+    }
+  }
+
+}
+
+/*==================================
+DISPLAY COLORS FUNCTION
+==================================*/
+
+const displayColorsBasedOnDesign = (designOption) => {
+
+  // get colors to sort
+  const colors = document.getElementById('color');
+
+  // depending on design chosen, sorts to display
+  // appropriate colors
+  if (designOption === 'js puns') {
+    jsPunsColors(colors);
+  } else if (designOption === 'heart js') {
+    heartJsColors(colors);
+  }
+
+}
+
+
+/*===============================================
+*** DOMContentLoaded Call Functions ***
+===============================================*/
 
 document.addEventListener('DOMContentLoaded', (e) => {
 
@@ -88,7 +180,11 @@ document.addEventListener('DOMContentLoaded', (e) => {
 
   // Create textarea if option other is selected
   selectJobRole.addEventListener('change', () => {
-    optionSelected(selectJobRole.value);
+    jobRoleOptionSelected(selectJobRole.value);
+  });
+
+  selectShirtDesign.addEventListener('change', () => {
+    displayColorsBasedOnDesign(selectShirtDesign.value);
   });
 
 });
