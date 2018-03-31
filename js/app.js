c41420f6ea5bf9f9a3553c05ad4532de5e5949c2
James_K_Conley <jkconleydec2012@gmail.com>
2018-03-31 12:30:36 -0700

    Added job role functionality for other option

--- js/app.js
+++ js/app.js
@@ -2,31 +2,93 @@
 VARIABLES
 ================*/
 
+// Elements on HTML DOC
 const nameField = document.getElementById('name');
+const basicInfoField = document.getElementById('basic-info');
+const selectJobRole = document.getElementById('title');
 
+// Elements created with js
+const div = document.createElement('div');
+const textArea = document.createElement('textarea');
 
-/*============
-FUNCTIONS
-============*/
+/*======================
+*** ONLOAD FUNCTIONS ***
+======================*/
 
-/*
+/*==================================
 FOCUS ON NAME FIELD WHEN PAGE LOADS
-*/
+==================================*/
 
-const focusOnNameFiled = () => {
+const focusOnNameField = () => {
   nameField.focus();
 }
 
+/*===================================================
+*** JOB ROLE FUNCTIONS ***
+===================================================*/
 
+/*=============================
+CREATE TEXTAREA
+=============================*/
+const createTextField = () => {
+
+  // Create textarea attr
+  const attrRows = document.createAttribute('rows');
+  const attrCols = document.createAttribute('cols');
+  const placeHolder = document.createAttribute('placeholder');
+  const textAreaId = document.createAttribute('id');
+
+  // assign attr values
+  attrRows.value = '5';
+  attrCols.value = '50';
+  placeHolder.value = 'Your Job Role';
+  textAreaId.value = 'other-title';
+
+  // set attr to textarea element
+  textArea.setAttributeNode(attrRows);
+  textArea.setAttributeNode(attrCols);
+  textArea.setAttributeNode(placeHolder);
+  textArea.setAttributeNode(textAreaId);
+
+  // append to the basic-info fieldset
+  basicInfoField.appendChild(div);
+  div.appendChild(textArea);
+
+}
+
+/*==================================
+CHECK WHICH OPTION IS SELECTED
+==================================*/
+
+const optionSelected = (option) => {
+
+  // assign textarea if present by id
+  const textArea = document.getElementById('other-title');
+
+  // check to see if textarea is null or created, then appear based
+  // on other option selected only
+  if (option === 'other' && textArea === null) {
+    createTextField();
+  } else if (textArea !== null && option !== 'other') {
+    textArea.style.display = 'none';
+  } else if (textArea !== null && option === 'other') {
+    textArea.style.display = '';
+  }
+}
 
 
 /*=============================
-DOMContentLoaded
+DOMContentLoaded Call Functions
 =============================*/
 
 document.addEventListener('DOMContentLoaded', (e) => {
 
-  // Focus on name field
-  focusOnNameFiled();
+  // Focus on name field onload
+  focusOnNameField();
+
+  // Create textarea if option other is selected
+  selectJobRole.addEventListener('change', () => {
+    optionSelected(selectJobRole.value);
+  });
 
 });
