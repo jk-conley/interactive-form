43b077e4bd9a55290e840b313072589df7c2fddb
James_K_Conley <jkconleydec2012@gmail.com>
2018-03-31 16:43:32 -0700

    Added the register activities functionality for total cost and if events conflict

--- js/app.js
+++ js/app.js
@@ -2,12 +2,27 @@
 VARIABLES
 ================*/
 
-// Elements on HTML DOC
+// focus variables
 const nameField = document.getElementById('name');
-const basicInfoField = document.getElementById('basic-info');
+
+// job role variables
 const selectJobRole = document.getElementById('title');
 const selectShirtDesign = document.getElementById('design');
 const textArea = document.getElementById('other-title');
+
+// register activity variables
+const activities = document.querySelectorAll('.activities > label');
+const activityFieldSet = document.querySelector('.activities');
+const div = document.createElement('div');
+const h3 = document.createElement('h3');
+const totalCostDiv = document.querySelector('.activities > div');
+const jsFrameworks = document.querySelector('.activities > label > input[name="js-frameworks"]');
+const express = document.querySelector('.activities > label > input[name="express"]');
+const jsLibs = document.querySelector('.activities > label> input[name="js-libs"]');
+const node = document.querySelector('.activities > label> input[name="node"]');
+
+
+
 
 /*======================
 *** ONLOAD FUNCTIONS ***
@@ -134,27 +149,119 @@ const displayColorsBasedOnDesign = (designOption) => {
 *** REGISTER ACTIVITY FUNCTIONS ***
 ===============================================*/
 
+/*==================================
+TOTAL COST FUNCTION
+==================================*/
+
+const activityTotalCost = (checkedList) => {
+
+  let total = 0;
+
+  for (let i = 0; i < checkedList.length; i++) {
+    console.log(checkedList[i].children[1].textContent);
+
+    if (checkedList[i].children[0].checked) {
+      total += parseInt(checkedList[i].children[1].textContent);
+    }
+  }
+
+  return total;
+}
+
+/*==================================
+CREATE TOTAL COST ELEMENT FUNCTION
+==================================*/
+
+const createTotalCostElement = (cost) => {
+
+  if (totalCostDiv === null) {
+    // append elements
+    activityFieldSet.appendChild(div);
+    div.appendChild(h3);
+    h3.textContent = `Total: $${cost}`;
+  } else if (totalCostDiv !== null) {
+    h3.textContent = `Total: $${cost}`;
+    totalCostDiv.style.display = '';
+  }
+
+}
+
+/*==================================
+REMOVE TOTAL COST ELEMENT FUNCTION
+==================================*/
+
+const removeTotalCostElement = () => {
+  if (totalCostDiv !== null) {
+    totalCostDiv.style.display = 'none';
+  }
+}
+
+/*==================================
+CHECK INPUT NAMES FUNCTION
+==================================*/
+
+const checkInputNames = (name, counterpart) => {
+
+  // check if input named checked conflicts with counterpart
+  if (name.checked) {
+    counterpart.disabled = true;
+    counterpart.parentNode.classList.add('disabled');
+  } else if (!name.checked) {
+    counterpart.disabled = false;
+    counterpart.parentNode.classList.remove('disabled');
+  }
+
+}
+
+/*==================================
+DO EVENTS CONFLICT FUNCTION
+==================================*/
+
+const doEventsConflict = () => {
+
+  // if jsFrameworks is checked disable express
+  checkInputNames(jsFrameworks, express);
+
+  // if express is checked disable jsFrameworks
+  checkInputNames(express, jsFrameworks);
+
+  // if jsLibs is checked disable node
+  checkInputNames(jsLibs, node);
+
+  // if node is checked disable jsLibs
+  checkInputNames(node, jsLibs);
+
+}
 
 
 /*===============================================
-*** DOMContentLoaded Call Functions ***
+*** DOMCONTENTLOADED CHECKED CALL FUNCTIONS ***
 ===============================================*/
 
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener('DOMContentLoaded', () => {
 
-  // Focus on name field onload
+  // focus on name field onload
   focusOnNameField();
 
   // hide other job role textarea
   textArea.style.display = 'none';
 
-  // Create textarea if option other is selected
+  // create textarea if option other is selected for job role
   selectJobRole.addEventListener('change', () => {
     jobRoleOptionSelected(selectJobRole.value);
   });
 
+  // listen for changes to design options and colors for t-shirts
   selectShirtDesign.addEventListener('change', () => {
     displayColorsBasedOnDesign(selectShirtDesign.value);
   });
+
+  // listen for activites checked in Register Activities Section
+  activityFieldSet.addEventListener('change', () => {
+    removeTotalCostElement();
+    createTotalCostElement(activityTotalCost(activities));
+    doEventsConflict();
+  });
+
 
 });
