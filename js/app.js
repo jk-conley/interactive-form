9cea3894c96f254a3fa3a8cbb4624e8c6a5bd791
James_K_Conley <jkconleydec2012@gmail.com>
2018-04-01 11:58:08 -0700

    Added shirt design functionality for color button to only appear when a design is selected.Added validation for name, email, and t-shirt sections

--- js/app.js
+++ js/app.js
@@ -7,13 +7,17 @@ const nameField = document.getElementById('name');
 
 // job role variables
 const selectJobRole = document.getElementById('title');
-const selectShirtDesign = document.getElementById('design');
 const textArea = document.getElementById('other-title');
+
+// t-shirt variables
+const selectColors = document.getElementById('colors-js-puns');
+const selectShirtDesign = document.getElementById('design');
+const shirtFieldSet = document.querySelector('.shirt');
 
 // register activity variables
 const activities = document.querySelectorAll('.activities > label');
 const activityFieldSet = document.querySelector('.activities');
-const div = document.createElement('div');
+const divTotal = document.createElement('div');
 const h3 = document.createElement('h3');
 const totalCostDiv = document.querySelector('.activities > div');
 const jsFrameworks = document.querySelector('.activities > label > input[name="js-frameworks"]');
@@ -26,6 +30,18 @@ const creditCard = document.getElementById('credit-card');
 const paypal = document.getElementById('paypal');
 const bitcoin = document.getElementById('bitcoin');
 const paymentOption = document.getElementById('payment');
+
+// validation variables
+const form = document.querySelector('form');
+const name = document.getElementById('name');
+const email = document.getElementById('mail');
+// Source for regular expression:
+// https://www.w3resource.com/javascript/form/email-validation.php
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const pRegError = document.createElement('p');
+const pShirtError = document.createElement('p');
+const pPaymentError = document.createElement('p');
+
 
 
 
@@ -63,6 +79,16 @@ const jobRoleOptionSelected = (option) => {
 /*===================================================
 *** T-SHIRT INFO FUNCTIONS ***
 ===================================================*/
+
+const isShirtDesignChosen = () => {
+  if (selectShirtDesign.value === 'js puns' || selectShirtDesign.value === 'heart js') {
+    selectColors.style.display = '';
+    return true;
+  } else {
+    selectColors.style.display = 'none';
+    return false;
+  }
+}
 
 /*==================================
 JS PUNS COLORS FUCNTION
@@ -182,8 +208,8 @@ const createTotalCostElement = (cost) => {
 
   if (totalCostDiv === null) {
     // append elements
-    activityFieldSet.appendChild(div);
-    div.appendChild(h3);
+    activityFieldSet.appendChild(divTotal);
+    divTotal.appendChild(h3);
     h3.textContent = `Total: $${cost}`;
   } else if (totalCostDiv !== null) {
     h3.textContent = `Total: $${cost}`;
@@ -272,21 +298,156 @@ const displayPaymentOption = () => {
 *** VALIDATION FUNCTIONS ***
 ===============================================*/
 
-const validation = () => {
-  
+/*==================================
+CREATE ERROR MESSAGE FUNCTION
+==================================*/
+
+const createErrorMessage = (element, p, message, id) => {
+  element.firstElementChild.append(p);
+  p.textContent = message;
+  p.setAttribute('class', 'validation-text');
+  p.setAttribute('id', id);
+  p.style.fontSize = '1rem';
 }
 
+/*==================================
+REMOVE ERROR MESSAGE FUNCTION
+==================================*/
+
+const removeErrorMessage = (parent, id) => {
+  const element = document.getElementById(id);
+  if (element !== null) {
+    parent.firstElementChild.remove();
+  }
+}
+
+/*==================================
+VALIDATE NAME FUNCTION
+==================================*/
+
+const validateName = () => {
+  // if name invalid set validation css classses and return false
+  if (name.value === '') {
+    name.classList.add('validation-box');
+    name.previousElementSibling.classList.add('validation-text');
+    return false;
+  } else {
+    name.classList.remove('validation-box');
+    name.previousElementSibling.classList.remove('validation-text');
+    return true;
+  }
+}
+
+/*==================================
+VALIDATE EMAIL FUNCTION
+==================================*/
+
+const validateEmail = () => {
+  // if no email or email is invalid set validation
+  // css classes and return false
+  console.log(mail.value);
+  console.log(emailRegex.test(mail.value));
+  if (mail.value === '' || !emailRegex.test(mail.value)) {
+    email.classList.add('validation-box');
+    email.previousElementSibling.classList.add('validation-text');
+    return false;
+  } else {
+    email.classList.remove('validation-box');
+    email.previousElementSibling.classList.remove('validation-text');
+    return true;
+  }
+
+}
+
+/*==================================
+VALIDATE SHIRT DESIGN FUNCTION
+==================================*/
+
+const validateShirtDesign = () => {
+
+  if (selectShirtDesign.value === 'js puns' || selectShirtDesign.value === 'heart js') {
+    return true;
+  } else {
+    return false
+  }
+
+}
+
+/*==================================
+VALIDATE REGISTRY FUNCTION
+==================================*/
+
+const validateRegistry = () => {
+
+  for (var i = 0; i < activities.length; i++) {
+    if (activities[i].children[0].checked) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
+
+
+
+
+/*==================================
+VALIDATES FORM FUNCTION
+==================================*/
+
+const validate = () => {
+
+  let flag;
+  // validate name field
+  if (validateName() === false) {
+    name.previousElementSibling.textContent = 'Name: Please enter your name';
+    flag = false;
+  }
+  // validate email field
+  if (validateEmail() === false) {
+    email.previousElementSibling.textContent = 'Email: Please enter a valid email';
+    flag = false;
+  }
+  // validate t-shirts for design selected
+  if (validateShirtDesign() === false) {
+    createErrorMessage(shirtFieldSet, pShirtError, "Don't forget your T-shirt!", 'shirt-error');
+  } else if (validateShirtDesign() === true) {
+    removeErrorMessage(shirtFieldSet.firstElementChild, 'shirt-error');
+  }
+
+  return flag;
+}
 /*===============================================
-*** DOMCONTENTLOADED CHECKED CALL FUNCTIONS ***
+*** CALL FUNCTIONS AND DOMCONTENTLOADED ***
 ===============================================*/
 
-document.addEventListener('DOMContentLoaded', () => {
+/*==================================
+INITIALIZE ON PAGE LOAD FUNCTION
+==================================*/
 
+const initialPageLoad = () => {
   // focus on name field onload
   focusOnNameField();
 
   // hide other job role textarea
   textArea.style.display = 'none';
+
+  // hide colors until design is chosen
+  isShirtDesignChosen();
+
+  // initial call to displayPaymentOption for setup
+  displayPaymentOption();
+}
+
+/*======================================
+DOMCONTENTLOADED CHECK RUN FUNCTIONS
+======================================*/
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // initialize page
+  initialPageLoad();
 
   // create textarea if option other is selected for job role
   selectJobRole.addEventListener('change', () => {
@@ -295,21 +456,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // listen for changes to design options and colors for t-shirts
   selectShirtDesign.addEventListener('change', () => {
+    isShirtDesignChosen();
     displayColorsBasedOnDesign(selectShirtDesign.value);
+    if (isShirtDesignChosen()) {
+      removeErrorMessage(shirtFieldSet.firstElementChild, 'shirt-error');
+    }
   });
 
   // listen for activites checked in Register Activities Section
   activityFieldSet.addEventListener('change', () => {
+    removeErrorMessage();
     removeTotalCostElement();
     createTotalCostElement(activityTotalCost(activities));
     doEventsConflict();
   });
 
-  // initial call to displayPaymentOption for setup
-  displayPaymentOption();
   // listen to payment section for option selected
   paymentOption.addEventListener('change', () => {
     displayPaymentOption();
+  });
+
+  // listen for form submition for validation check
+  form.addEventListener('submit', (e) =>{
+    if (validate() === false) {
+      e.preventDefault();
+    }
   });
 
 
